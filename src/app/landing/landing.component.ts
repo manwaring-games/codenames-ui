3bf975c30afbba787ee685f6b8263d23463658f3
Andrew <andrew.manwaring@gmail.com>
2020-05-24 00:14:35 -0500

    Submit username if joining game

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-landing',
@@ -7,11 +8,25 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-  gameCodeEntry = new FormControl('');
+  @ViewChild('joinGameModal')
+  private joinGameModal: TemplateRef<any>;
 
-  constructor() { }
+  gameCodeEntry = new FormControl('');
+  username = new FormControl('');
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  onJoinGameClick() {
+    this.modalService.open(this.joinGameModal, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      console.log(this.gameCodeEntry.value);
+      console.log(this.username.value);
+    }, (reason) => {
+      console.log(this.gameCodeEntry.value);
+      console.log(this.username.value);
+    });
   }
 
 }
